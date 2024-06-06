a1900d68a0ff50226f02135cf31bbf97f5dcefa2
Sooraj-CR <sorajraju17@gmail.com>
2024-06-06 22:51:25 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { TodoWrap } from "./Components/TodoWrap";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <TodoWrap />
+    </div>
+  );
+};
+
+export default App;
